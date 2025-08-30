fc1dfdfabdeb5aa1ecff0651d544e513cd621670
Yash Golani <golaniyash.yg@gmail.com>
2025-08-30 20:02:17 +0530

    test: negative numbers should throw with list

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -18,3 +18,6 @@ test('handles newlines as delimiters: "1\\n2,3" => 6', () => {
 test('supports custom single-character delimiter: "//;\\n1;2" => 3', () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+test("throws exception for negative numbers", () => {
+  expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+});
