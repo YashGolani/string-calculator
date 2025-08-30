5759a2bc2d17b79e67ff0d2d854c8001a31943cf
Yash Golani <golaniyash.yg@gmail.com>
2025-08-30 19:45:29 +0530

    test: add single number returns value

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,6 @@ const { add } = require("../src/stringCalculator");
 test('add("") should return 0', () => {
   expect(add("")).toBe(0);
 });
+test('add("5") should return 5', () => {
+  expect(add("5")).toBe(5);
+});
