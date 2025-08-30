65840935022ddd36470ec8895d5a860167636740
Yash Golani <golaniyash.yg@gmail.com>
2025-08-30 20:00:37 +0530

    feat: support newline as delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,9 @@
 function add(numbers) {
-  if (numbers === "") return 0;
+  if (!numbers) return 0;
+
+  // split by comma or newline
   const parts = numbers
-    .split(",")
+    .split(/,|\n/)
     .map((n) => parseInt(n, 10))
     .filter((n) => !Number.isNaN(n));
   return parts.reduce((s, n) => s + n, 0);
