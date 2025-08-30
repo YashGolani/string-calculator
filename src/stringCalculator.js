037a06b2605f503f0cccb4984bcb76115f6b039e
Yash Golani <golaniyash.yg@gmail.com>
2025-08-30 19:50:08 +0530

    feat: support comma-delimited numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,15 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  // single number
-  return parseInt(numbers, 10);
+
+  // handle single number
+  if (!numbers.includes(",")) {
+    return parseInt(numbers, 10);
+  }
+
+  return numbers
+    .split(",")
+    .map((n) => parseInt(n, 10))
+    .reduce((s, n) => s + n, 0);
 }
 
 module.exports = { add };
