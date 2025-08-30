e1f486583cd761342af696ca4145208aabe6348e
Yash Golani <golaniyash.yg@gmail.com>
2025-08-30 20:01:49 +0530

    feat: custom single-character delimiter support

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,9 +1,21 @@
+function escapeRegExp(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
 function add(numbers) {
   if (!numbers) return 0;
 
-  // split by comma or newline
+  let delimiters = /,|\n/;
+  // custom delimiter header
+  if (numbers.startsWith("//")) {
+    const firstLineEnd = numbers.indexOf("\n");
+    const delimiterSection = numbers.substring(2, firstLineEnd); // e.g. ";"
+    delimiters = new RegExp(escapeRegExp(delimiterSection));
+    numbers = numbers.substring(firstLineEnd + 1);
+  }
+
   const parts = numbers
-    .split(/,|\n/)
+    .split(delimiters)
     .map((n) => parseInt(n, 10))
     .filter((n) => !Number.isNaN(n));
   return parts.reduce((s, n) => s + n, 0);
